b67c9a15a0407a7c6e12ec6b4a8c432f41d24965
Kristiana Ingco <kristiana.d.ingco@gmail.com>
2024-03-08 11:41:54 -0800

    Add test suite and tests for checking quantity of products in warehouses

---
+++ cypress/e2e/post requests/checkQuantityOfGoodsInWarehouse.cy.js
@@ -0,0 +1,72 @@
+/// <reference types="Cypress"/>
+
+describe("Check Quantity of Goods in Warehouse", () => {
+    let data;
+
+    before(() => {
+        cy.fixture("idsForQuantityCheck").then((idsForQuantityCheckData) => {
+            data = idsForQuantityCheckData;
+        });
+    });
+
+    beforeEach(() => {
+        cy.request({
+            url: "/api/v1/warehouses/amount",
+            method: "POST",
+            body: {
+                ids: data.existingID,
+            },
+        }).as("existingItemQuantity");
+
+        cy.request({
+            url: "/api/v1/warehouses/amount",
+            method: "POST",
+            body: {
+                ids: data.nonExistingID,
+            },
+            failOnStatusCode: false,
+        }).as("nonExistingItemQuantity");
+    });
+
+    // Successfully checking the quantity of goods in warehouse
+    it("should return a status code of 200 when item ID exists", () => {
+        cy.get("@existingItemQuantity").its("status").should("equal", 200);
+    });
+
+    it("should have 'Everything You Need' property when item ID exists", () => {
+        cy.get("@existingItemQuantity").then((res) => {
+            expect(res.body).to.have.property("Everything You Need");
+        });
+    });
+
+    it("should have 'Food City' property when item ID exists", () => {
+        cy.get("@existingItemQuantity").then((res) => {
+            expect(res.body).to.have.property("Food City");
+        });
+    });
+
+    it("should have 'Big World' property when item ID exists", () => {
+        cy.get("@existingItemQuantity").then((res) => {
+            expect(res.body).to.have.property("Big World");
+        });
+    });
+
+    it("should have 'Fresh Food' property when item ID exists", () => {
+        cy.get("@existingItemQuantity").then((res) => {
+            expect(res.body).to.have.property("Fresh Food");
+        });
+    });
+
+    // Checking the quantity of goods in warehouse when product id does not exist
+
+    // both tests below are failing, resulting in 500 status code and no message property. below are skipped for other tests to continue
+    it.skip("should return a status code of 404 when item ID does not exist", () => {
+        cy.get("@nonExistingItemQuantity").its("status").should("equal", 404);
+    });
+
+    it.skip("should have message' property when item ID does not exist", () => {
+        cy.get("@nonExistingItemQuantity").then((res) => {
+            expect(res.body).to.have.property("message");
+        });
+    });
+});
