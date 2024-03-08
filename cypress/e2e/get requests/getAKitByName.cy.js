c9b3f10c082d53b2d2957e464992ed5a018c968f
Kristiana Ingco <kristiana.d.ingco@gmail.com>
2024-03-07 19:43:28 -0800

    Add tests for expecting properties in getting a kit by name

--- cypress/e2e/get requests/getAKitByName.cy.js
+++ cypress/e2e/get requests/getAKitByName.cy.js
@@ -20,7 +20,32 @@ describe("Get a Kit By Name", () => {
         }).as("failingSearch");
     });
 
-    it("passes", () => {
-        cy.visit("https://example.cypress.io");
+    // Successfully receiving a kit
+    it("should return a status code of 200 when kit is found", () => {
+        cy.get("@successfulSearch").its("status").should("equal", 200);
+    });
+
+    it("should have 'id' property when kit is found", () => {
+        cy.get("@successfulSearch").then((res) => {
+            expect(res.body).to.have.property("id");
+        });
+    });
+
+    it("should have 'name' property when kit is found", () => {
+        cy.get("@successfulSearch").then((res) => {
+            expect(res.body).to.have.property("name");
+        });
+    });
+
+    it("should have 'productsList' property when kit is found", () => {
+        cy.get("@successfulSearch").then((res) => {
+            expect(res.body).to.have.property("productsList");
+        });
+    });
+
+    it("should have 'productsCount' property when kit is found", () => {
+        cy.get("@successfulSearch").then((res) => {
+            expect(res.body).to.have.property("productsList");
+        });
     });
 });
